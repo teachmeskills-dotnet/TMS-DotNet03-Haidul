8f91387f70472aba3e053961548564f0e9a45e54
Dakamakat <gazon-ozon@yandex.ru>
2020-10-01 00:16:56 +0300

    feat : add application context with identity

---
+++ src/EventMaker.DAL/Context/EventMakerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventMaker.DAL.Context
+{
+    /// <summary>
+    /// Database context.
+    /// </summary>
+    public  class EventMakerDbContext : IdentityDbContext
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="options"></param>
+        public EventMakerDbContext(DbContextOptions<EventMakerDbContext> options)
+            : base(options) { }
+         
+    }
+}
+  
